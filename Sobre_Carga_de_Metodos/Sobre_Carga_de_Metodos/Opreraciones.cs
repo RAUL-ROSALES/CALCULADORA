34d76d44d75308cb5a528cded638c4120282b29f
RAUL-ROSALES <55122080+RAUL-ROSALES@users.noreply.github.com>
2019-10-01 17:41:42 -0700

    Add files via upload

---
+++ Sobre_Carga_de_Metodos/Sobre_Carga_de_Metodos/Opreraciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobre_Carga_de_Metodos
+{
+   public class Opreraciones
+    {
+        public double Sumar(double a)
+        {
+            double res;
+            res = a + 7;
+            return res;
+        }//creo tres metodos que sean del mismo tipo 
+        //pero con diferentes parametros 
+        public double Sumar(double a, double b)
+        {
+            double res;
+            res = a + b;
+            return res;
+        }
+        public double Sumar()
+        {
+            double res;
+            res = 1 + 2;
+            return res;
+        }
+    }
+}
---
+++ Sobre_Carga_de_Metodos/Sobre_Carga_de_Metodos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobre_Carga_de_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opreraciones m1 = new Opreraciones();
+            double res1 = m1.Sumar();//aqui toma el valor de el metodo suma
+            double res2 = m1.Sumar(1, 2);//no ocupo istanciar varias veces 
+            double res3 = m1.Sumar(1);
+            Console.WriteLine("resultado 1: " + res1);
+            Console.WriteLine("resultado 2: " + res2);
+            Console.WriteLine("resultado 3: " + res3);
+            principal d = new principal();//instancio la clase Datos para poder acceder al metodo obtener datos
+            // y que ejecute todo lo que tiene el medoto 
+            d.Obtener_Datos();
+            Console.ReadKey();
+        }
+    }
+}
